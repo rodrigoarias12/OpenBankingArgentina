4381265cdce30bb58a973b4222098ab764693089
rodrigoarias12 <rodrigogonzaloarias@gmail.com>
2020-12-15 14:20:55 -0300

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,7 +20,7 @@ module.exports = {
           label: 'Docs API',
           position: 'left',
         },
-        //{to: 'blog', label: 'Blog', position: 'left'},
+        {to: 'blog', label: 'Blog', position: 'left'},
         {
           href: 'https://github.com/rodrigoarias12/OpenBankingArgentinaDocusaurus',
           label: 'GitHub',
