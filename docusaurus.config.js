3d78921fa75931611cb109a7055c3641912b122f
rodrigoarias12 <rodrigogonzaloarias@gmail.com>
2020-12-30 20:56:44 -0300

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -3,8 +3,6 @@ module.exports = {
   tagline: '',
   url: 'https://your-docusaurus-test-site.com',
   baseUrl: '/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
   organizationName: 'facebook', // Usually your GitHub org/user name.
   projectName: 'docusaurus', // Usually your repo name.
